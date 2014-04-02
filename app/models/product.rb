963c32fb9bc0b4aacc070d356d9e78b1c3e94382
boyang-li <bryanli2009@live.ca>
2014-04-01 23:21:24 -0400

    Add validation for product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,28 @@
 class Product < ActiveRecord::Base
 	attr_accessor :title,
-								:description,
-								:active,
-								:min,
-								:max,
-								:start_time,
-								:end_time,
-								:expire_on,
-								:regular_price,
-								:actual_price,
-								:product_fee,
-								:currency,
-								:image_url,
-								:require_shipping,
-								:categories_id
+		:description,
+		:active,
+		:min,
+		:max,
+		:start_time,
+		:end_time,
+		:expire_on,
+		:regular_price,
+		:actual_price,
+		:product_fee,
+		:currency,
+		:image_url,
+		:require_shipping,
+		:categories_id
+
+	validates: :title, :description, :min, :max, :start_time, :end_time, :expire_on, :regular_price, 
+		:actual_price, :product_fee, :image_url, presence: :ture
+	validates: :regular_price, :actual_price, :product_fee, numerically: {greater_than_or_equal_to: 0.01}
+	validates: :title, uniqueness: :true
+	validates: :image_url, allow_blank: :true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: "must be an url for GIF, JPG or PNG image."
+	}
 
 	def initialize(attributes = {})
 		attributes.each do |k, v|
