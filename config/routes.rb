5f3446adc66626c54d84fb9493a2c3f15763a1d5
boyang-li <bryanli2009@live.ca>
2014-04-27 22:20:03 -0400

    add store controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
+  resources :products
+
+  root 'store#index', as: 'store'
+
   devise_for :users, path: "auth", path_names: {sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification',
     unlock: 'unblock', registration: 'register'}
-
-  root "products#index"
-  
-  resources :products
-  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,104 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Manufacturer.delete_all
-manufacturer = Manufacturer.create!(:name=> "Intel", :image=> "image.jpg")
-
 Product.delete_all
-product = Product.create!(model: "i7-4770K", sku: "sku", mpn: "mpn", upc: "upc", ean: "ean", jan: "jan", isbn: "isbn", location: "Toronto", quantity: 20, image: "image.jpg", 
-   price: 369.0, points: 100, date_available: DateTime.now, minimum: 1, manufacturer_id: manufacturer.id)
-
 ProductDescription.delete_all
-ProductDescription.create!(product_id: product.id, name: "Intel Core i7-4770K Quad-Core Processor", description: "Core i7 Quad-core i7-4770K 3.5GHz Desktop Processor",
-  meta_description: "Intel i7 Quad-core Processor", meta_keyword: "Intel i7 Quad-core Processor", tag: "Intel i7 Quad-core Processor")
+
+manufacturer = Manufacturer.create!(name: "Apple Inc.", image: "apple.png")
+
+# test product #1
+product = Product.create!(
+  model: "model",
+  sku: "sku1",
+  mpn: "mpn",
+  upc: "upc",
+  ean: "ean",
+  jan: "jan",
+  isbn: "isbn",
+  location: "Toronto",
+  quantity: 10,
+  image: "products/macbookpro-13-retina-select-2013.png",
+  price: 1349.00,
+  points: 1000,
+  date_available: DateTime.now,
+  minimum: 1,
+  manufacturer_id: manufacturer.id)
+
+ProductDescription.create!(
+  product_id: product.id,
+  name: "MacBook Pro 13-inch: 2.4GHz with Retina display",
+  description: %{<ul>
+  <li>2.4GHz dual-core Intel Core i5</li>
+  <li>Turbo Boost up to 2.9GHz</li>
+  <li>4GB 1600MHz memory</li>
+  <li>128GB PCIe-based flash storage1</li>
+  <li>Intel Iris Graphics</li>
+  <li>Built-in battery (9 hours)</li>
+  </ul>},
+  meta_description: "MacBook Pro 13-inch: 2.4GHz with Retina display",
+  meta_keyword: "MacBook Pro",
+  tag: "MacBook Pro")
+
+# test product #2
+product = Product.create!(
+  model: "model",
+  sku: "sku2",
+  mpn: "mpn",
+  upc: "upc",
+  ean: "ean",
+  jan: "jan",
+  isbn: "isbn",
+  location: "Toronto",
+  quantity: 20,
+  image: "products/macbook-air-11-step1-hero-2013.png",
+  price: 999.00,
+  points: 1000,
+  date_available: DateTime.now,
+  minimum: 1,
+  manufacturer_id: manufacturer.id)
+
+ProductDescription.create!(
+  product_id: product.id,
+  name: "MacBook Air 11-inch : 128GB",
+  description: %{<ul>
+  <li>1.3GHz dual-core Intel Core i5 processor</li>
+  <li>Turbo Boost up to 2.6GHz</li>
+  <li>Intel HD Graphics 5000</li>
+  <li>4GB memory</li>
+  <li>128GB flash storage</li>
+  </ul>},
+  meta_description: "MacBook Air 11-inch : 128GB",
+  meta_keyword: "MacBook Air",
+  tag: "MacBook Air")
+
+# test product #3
+product = Product.create!(
+  model: "model",
+  sku: "sku3",
+  mpn: "mpn",
+  upc: "upc",
+  ean: "ean",
+  jan: "jan",
+  isbn: "isbn",
+  location: "Toronto",
+  quantity: 20,
+  image: "products/macbookpro-13-retina-select-2013.png",
+  price: 1349.00,
+  points: 1000,
+  date_available: DateTime.now,
+  minimum: 1,
+  manufacturer_id: manufacturer.id)
+
+ProductDescription.create!(
+  product_id: product.id,
+  name: "iMac 27-inch: 3.2GHz",
+  description: %{<ul>
+  <li>3.2GHz quad-core Intel Core i5</li>
+  <li>Turbo Boost up to 3.6GHz</li>
+  <li>8GB (two 4GB) memory</li>
+  <li>1TB hard drive</li>
+  <li>NVIDIA GeForce GT 755M with 1GB video memory</li>
+  </ul>},
+  meta_description: "iMac",
+  meta_keyword: "iMac",
+  tag: "Intel i7 Quad-core Processor")
