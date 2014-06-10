37613e1943ec00023e26e281f0693c9697acc443
boyang-li <bryanli2009@live.ca>
2014-06-10 00:35:12 -0400

    Add show view for carts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610033628) do
+ActiveRecord::Schema.define(version: 20140610042143) do
 
   create_table "attribute_descriptions", force: true do |t|
     t.integer "attribute_id"
